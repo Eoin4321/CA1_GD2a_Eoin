1f7980b11ccf9b87f2b397442b136fc3c70375f4
Eoin4321 <eoinhamill1919@gmail.com>
2023-10-22 15:55:40 +0100

    Added invincibility and red flash for my character after they take damage. Created prefabs for Collectibles and Traps.
    Added invincibility for my character after they take damage. Added in a red flash for when they take damage to clearly indicate to the player that they have taken damage.
    
    Created a prefab for my Collectibles and Traps
    
    Moved bullets to foreground to fix issue when they are not visible.

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
    
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("Invinibility Frames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfflashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
@@ -23,6 +31,7 @@ public class Health : MonoBehaviour
         {
             //player damaged;
             anim.SetTrigger("hurt");
+            StartCoroutine(Invunerability());
             //Infinibility frames
         }
         else
@@ -40,5 +49,18 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(10,11, true);
+        for (int i = 0; i < numberOfflashes; i++) 
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfflashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfflashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
