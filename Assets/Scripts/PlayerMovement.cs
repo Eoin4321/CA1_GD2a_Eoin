108d5de227794e2ad9b157fd2c34014addebca8a
Eoin4321 <eoinhamill1919@gmail.com>
2023-10-24 22:59:12 +0100

    Added in weighted jump and coyote time. Commented out dash code
    Added in weighted jump so player jumps depending on how long they hold down the jump input.
    Added in coyote time so the player has time to jump after they jump off the edge.
    Added in code for dash but it is currently not working so I commented it out.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]private float speed;
-    [SerializeField]private float jumpPower;
-    [SerializeField]private LayerMask groundLayer;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform wallCheck;
     [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Transform roofCheck;
+    [SerializeField] private LayerMask roofLayer;
+    [SerializeField] private TrailRenderer tr;
+
+    /*private bool canDash = true;
+    [SerializeField] private bool isDashing;
+    [SerializeField] private float dashingPower;
+    [SerializeField] private float dashingTime = 0.2f;
+    [SerializeField] private float dashingCooldown = 1f;
+    */
+
+
+    [Header("Coyote Time")]
+    private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D boxCollider;
@@ -36,29 +55,60 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If player is dashing locks out of other movements
+        /*
+        if(isDashing)
+        {
+            return;
+        }
+
+
+
+        if(Input.GetKeyDown(KeyCode.Mouse1) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+        */
         //This stores the HorizontalInput
         horizontalInput = Input.GetAxis("Horizontal");
+
+        if (isGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
 
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
         //Checking to see what way the player is facing if they are facing left it will
         //change the sprite to look the other way.
-        if (horizontalInput >0.01f)
-            transform.localScale = new Vector3(currentsize,currentsize,1);
-        else if(horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-(currentsize),currentsize,1);
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(currentsize, currentsize, 1);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-(currentsize), currentsize, 1);
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded())
+        if (Input.GetKeyDown(KeyCode.Space) && coyoteTimeCounter > 0)
             Jump();
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && currentsize == 5 && isWall() == false)
+        if (Input.GetKeyUp(KeyCode.Space) && body.velocity.y > 0)
+        { 
+        body.velocity = new Vector2(body.velocity.x, body.velocity.y / 2);
+        coyoteTimeCounter = 0f;//Prevents player from double jumping by spamming the jump button
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && currentsize == 5)
         {
             currentsize = 2;
             transform.localScale = new Vector3(currentsize, currentsize, 1);
+           
         }
-        else if (Input.GetKeyDown(KeyCode.LeftShift) && currentsize == 2 && isWall()==false)
+        else if (Input.GetKeyDown(KeyCode.LeftShift) && currentsize == 2 && isWall() == false && isRoof() == false)
         {
             currentsize = 5;
             transform.localScale = new Vector3(currentsize, currentsize, 1);
+            
         }
 
             //Set animator parameters 
@@ -71,23 +121,46 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, jumpPower);
-        anim.SetTrigger("jump");
+        
        
     }
 
     private bool isGrounded()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
+
     private bool isWall()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
-        return raycastHit.collider != null;
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private bool isRoof()
+    {
+        return Physics2D.OverlapCircle(roofCheck.position, 0.2f, groundLayer);
     }
 
     public bool canAttack()
     {
         return isGrounded();
     }
+
+    /*private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = body.gravityScale;
+        body.gravityScale = 0f;
+        body.velocity = new 
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        body.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        
+        canDash = true;
+
+
+    }*/
 }
